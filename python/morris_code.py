556da70b03db4971cbbb809423d3a4cf1e7afdb6
paulromer12 <paulromer2036@gmail.com>
2024-08-18 15:55:01 -0500

    started morris code translator in python

--- python/morris_code.py
+++ python/morris_code.py
@@ -1,12 +1,67 @@
-to_translate = input('Enter the english or morris code you want translated?')
+# todo: known issue, the code doesn't keep the spaces in the words
 
-if to_translate[0] == '-':
-    translateMorris(to_translate)
-else:
-    translateEnglish(to_translate)
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+translation_dict = {
+    "a": ".-",
+    "b": "-...",
+    "c": "-.-.",
+    "d": "-..",
+    "e": ".",
+    "f": "..-.",
+    "g": "--.",
+    "h": "....",
+    "i": "..",
+    "j": ".---",
+    "k": "-.-",
+    "l": ".-..",
+    "m": "--",
+    "n": "-.",
+    "o": "---",
+    "p": ".--.",
+    "q": "--.-",
+    "r": ".-.",
+    "s": "...",
+    "t": "-",
+    "u": "..-",
+    "v": "...-",
+    "w": ".--",
+    "x": "-..-",
+    "y": "-.--",
+    "z": "--.."
+}
 
+# translate from English to Morris Code:
+def translate_to_morris(english_str):
+    morris_code = ""
+    for char in english_str.lower():
+        if char == ' ':
+            continue
+        elif char in translation_dict:
+            morris_code += translation_dict[char] + " "
+        else:
+            print(f"Warning: Character '{char}' not found in Morris Code dictionary.")
+    return morris_code
 
-def translateEnglish(to_translate):
+# translate from Morris Code to English:
+def translate_to_english(morris_str):
+    english = ""
+    for code in morris_str.split():
+        if code == '0':
+            english += " "
+        elif code in translation_dict.values():
+            key = [k for k, v in translation_dict.items() if v == code]
+            english += key[0]
+        else:
+            print(f"Warning: Code '{code}' not found in Morris Code dictionary.")
+    return english
 
+input_str = input('Enter the english or morris code you want translated? ')
 
-def translateMorris(to_translate):
+try:
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    if input_str[0].lower() in alphabet:
+        print(translate_to_morris(input_str))
+    else:
+        print(translate_to_english(input_str))
+except ValueError as e:
+    print(f"Error: {e}")
